209e7330f698f01767fe7f6546df11bb5a7abfd0
Gabriel Duarte <gabrield.developer@gmail.com>
2021-02-28 11:42:38 -0300

    feat: create basic routes for the application

--- quiz-app/src/app/app-routing.module.ts
+++ quiz-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './features/quiz/quiz.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  component: HomeComponent
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
+import { QuizComponent } from './features/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
